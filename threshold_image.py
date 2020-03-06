456ae18f17a90469edb273c89e454252b6538ac7
minhchau97 <61772618+minhchau97@users.noreply.github.com>
2020-03-06 14:06:43 +0700

    Create threshold_image.py

---
+++ threshold_image.py
@@ -0,0 +1,19 @@
+
+import cv2
+import numpy as np
+
+img = cv2.imread('gradient.png')
+_,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY) # ngưỡng ảnh binary chỉ 0 và 1 trắng và đen
+_,th2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV) # ngưỡng ảnh binary đảo
+_,th3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC) # cắt ngắn ngưỡng 1
+_,th4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO) # tăng cường ngưỡng 1 và giảm thiểu ngưỡng 0
+_,th5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV) # giảm mức 1 giữ mức 0
+cv2.imshow('Thesh_Ogriginal',img)
+cv2.imshow('Thesh_Binary',th1)
+cv2.imshow('Thesh_Binary_IVT',th2)
+cv2.imshow('Thesh_Trunc',th3)
+cv2.imshow('Thesh_To_Zero',th4)
+cv2.imshow('Thesh_To_Zero_IVT',th5)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
