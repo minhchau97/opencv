390bb7c906648d19db2c1c5fa3aaa1d2f3afdce5
minhchau97 <61772618+minhchau97@users.noreply.github.com>
2020-03-06 15:06:43 +0700

    Update threshold_image.py

--- threshold_image.py
+++ threshold_image.py
@@ -17,3 +17,21 @@ cv2.imshow('Thesh_To_Zero_IVT',th5)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# ADAPTIVE_THRESH_
+
+
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('sudoku.png',0)
+_, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2); # thay đổi ngưỡng
+th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2); # làm mờ
+
+cv.imshow("Image", img)
+cv.imshow("THRESH_BINARY", th1)
+cv.imshow("ADAPTIVE_THRESH_MEAN_C", th2)
+cv.imshow("ADAPTIVE_THRESH_GAUSSIAN_C", th3)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
